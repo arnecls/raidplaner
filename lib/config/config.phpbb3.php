e443f5a4f389f4692903da7207e8a2bce8e11c0a
Arne Claus <arne.claus@gmail.com>
2011-03-05 22:56:34 +0000

    .

--- lib/config/config.phpbb3.php
+++ lib/config/config.phpbb3.php
@@ -4,8 +4,6 @@
 	define("PHPBB3_TABLE", "packedpixel1");
 	define("PHPBB3_USER", "root");
 	define("PHPBB3_PASS", "root");
-	//define("PHPBB3_USER", "packedpixel");
-	//define("PHPBB3_PASS", "i948hoBWGBUS9");
 	define("PHPBB3_TABLE_PREFIX", "phpbb_");
 	
 	define("PHPBB3_MEMBER_GROUPS", "7, 8" );
