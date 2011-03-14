569a46c790d4b3ddb6915f4d6bc8fef178e6e522
Arne Claus <arne.claus@gmail.com>
2011-03-14 07:29:48 +0000

    REMOVED: Config files as setup now generates them

--- lib/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define("SQL_HOST", "localhost");
-	define("RP_DATABASE", "raidplaner");
-	define("RP_USER", "root");
-	define("RP_PASS", "root");
-	define("RP_TABLE_PREFIX", "raid_");
-	define("ALLOW_REGISTRATION", true);
-?>
--- lib/config/config.phpbb3.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("PHPBB3_BINDING", true);
-	define("PHPBB3_DATABASE", "phpbb");
-	define("PHPBB3_USER", "root");
-	define("PHPBB3_PASS", "root");
-	define("PHPBB3_TABLE_PREFIX", "phpbb_");
-	define("PHPBB3_MEMBER_GROUPS", "");
-	define("PHPBB3_RAIDLEAD_GROUPS", "");
-?>
