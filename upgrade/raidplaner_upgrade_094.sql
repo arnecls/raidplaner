67b669a9bf23f5af0b4828165ad77f5b1c4640cc
Arne Claus <arne.claus@gmail.com>
2012-04-22 14:51:59 +0000

    MODIFIED: removed upgrade folder

--- upgrade/raidplaner_upgrade_094.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE  `raid_User` ADD  `Created` DATETIME NOT NULL;
-INSERT INTO `raid_Setting` (`SettingId`, `Name`, `IntValue`, `TextValue`) VALUES (NULL, 'RaidStartHour', '19', ''), (NULL, 'RaidStartMinute', '30', '');
-INSERT INTO `raid_Setting` (`SettingId`, `Name`, `IntValue`, `TextValue`) VALUES (NULL, 'RaidEndHour', '23', ''), (NULL, 'RaidEndMinute', '0', '');
-INSERT INTO `raid_Setting` (`SettingId`, `Name`, `IntValue`, `TextValue`) VALUES (NULL, 'RaidSize', '10', '');
-INSERT INTO `raid_Setting` (`SettingId`, `Name`, `IntValue`, `TextValue`) VALUES (NULL, 'Site', '', ''), (NULL, 'Banner', '', 'cata');
-INSERT INTO `raid_Setting` (`SettingId`, `Name`, `IntValue`, `TextValue`) VALUES (NULL, 'Version', '94', '');
