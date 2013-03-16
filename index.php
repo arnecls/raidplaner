ab9ab087318cb0bd89a789affc4f12999f655e4f
Arne Claus <arne.claus@gmail.com>
2013-03-16 17:39:03 +0000

    MODIFIED: Version set to 0.9.7a

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     UserProxy::GetInstance(); // Init user
     loadSiteSettings();
 		     
-    $siteVersion = 97;
+    $siteVersion = 97.1;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
