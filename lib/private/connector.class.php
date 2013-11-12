c6d7234c44ee6b28768bf164b7bcf6c722e67510
Arne Claus <arne.claus@gmail.com>
2013-11-12 19:28:28 +0000

    MODIFIED: Removed MySQL UTC enforcing (will be enabled with 1.1)

--- lib/private/connector.class.php
+++ lib/private/connector.class.php
@@ -20,7 +20,8 @@
                 $this->mHost  = $aHost;
                 $this->mDatabase = $aDatabase;
                 parent::__construct("mysql:dbname=".$aDatabase.";host=".$aHost, $aUser, $aPass,
-                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8', time_zone = '+00:00'"));
+                	array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+                    //array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8', time_zone = '+00:00'"));
             }
             catch (PDOException $Exception)
             {
