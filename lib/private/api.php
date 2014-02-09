c719449c8ea741a2d993195fbd7d73f3ea77eb6a
Arne Claus <arne.claus@gmail.com>
2014-02-09 20:21:49 +0100

    api framework

--- lib/private/api.php
+++ lib/private/api.php
@@ -1,12 +1,21 @@
 <?php
 
     require_once(dirname(__FILE__)."/settings.class.php");
+    require_once(dirname(__FILE__)."/api_help.php");
+    require_once(dirname(__FILE__)."/api_raid.php");
+    require_once(dirname(__FILE__)."/api_location.php");
+    require_once(dirname(__FILE__)."/api_statistic.php");
 
     class Api
     {
+        private static $PublicUpdateInterval = 2; // Seconds until new public key
+    
+        // ---------------------------------------------------------------------
+                
         public static function getPrivateToken()
         {
             $Settings = Settings::getInstance();
+            
             if (!isset($Settings["ApiPrivate"])) 
             {
                 $PrivateToken = dechex(crc32(openssl_random_pseudo_bytes(2048))).
@@ -18,7 +27,7 @@
                 );
             }
             
-            return $Settings["ApiPrivate"];
+            return $Settings["ApiPrivate"]["TextValue"];
         }
         
         // ---------------------------------------------------------------------
@@ -27,18 +36,87 @@
         {
             $Settings = Settings::getInstance();
             
-            if (!isset($Settings["ApiPublic"]) || 
-                ($Settings["ApiPublic"]["IntValue"] < time())) 
+            if (!isset($Settings["ApiPublic"])) 
+                self::tryGeneratePublicToken();
+            
+            return $Settings["ApiPublic"]["TextValue"];
+        }
+        
+        // ---------------------------------------------------------------------
+        
+        public static function tryGeneratePublicToken()
+        {
+            $Settings = Settings::getInstance();
+            
+            if ($Settings["ApiPublic"]["IntValue"] < time()) 
             {
                 $PublicToken = md5(openssl_random_pseudo_bytes(2048));
                 
+                // Store old token so that running requests don't fail
+                
+                if (isset($Settings["ApiPublic"]))
+                {
+                    $Settings["ApiPublicOld"] = Array(
+                        "IntValue"  => $Settings["ApiPublic"]["IntValue"],
+                        "TextValue" => $Settings["ApiPublic"]["TextValue"]
+                    );
+                }
+                
+                // New token
+                
                 $Settings["ApiPublic"] = Array(
-                    "IntValue"  => time() + 60,
+                    "IntValue"  => time() + self::$PublicUpdateInterval,
                     "TextValue" => $PublicToken
                 );
             }
+        }
+        
+        // ---------------------------------------------------------------------
+        
+        public static function testPrivateToken($aToken)
+        {
+            return self::getPrivateToken() == $aToken;
+        }
+        
+        // ---------------------------------------------------------------------
+        
+        public static function testPublicToken($aToken)
+        {
+            $Settings = Settings::getInstance();
             
-            return $Settings["ApiPublic"]["TextValue"];
+            // Make sure token is regenerated on a regular basis
+            
+            self::tryGeneratePublicToken();
+            
+            if ($Settings["ApiPublic"]["TextValue"] == $aToken)
+                return true;
+                
+            // Make sure to test the timeout of old values (long-time update gap)
+                
+            return (isset($Settings["ApiPublicOld"]) &&
+                ($Settings["ApiPublicOld"]["IntValue"] + self::$PublicUpdateInterval > time()) &&
+                ($Settings["ApiPublicOld"]["TextValue"] == $aToken));
+        }
+        
+        // ---------------------------------------------------------------------
+        
+        public static function queryLocations()
+        {
+            return api_query_location();
+        }
+        
+        // ---------------------------------------------------------------------
+        
+        public static function queryRaid($aParameter)
+        {
+            return api_query_raid($aParameter);
+        }
+        
+        // ---------------------------------------------------------------------
+        
+        public static function queryStatistic($aParameter)
+        {
+            return api_query_statistic($aParameter);
         }
     }
 
---
+++ lib/private/api_help.php
@@ -0,0 +1,61 @@
+<?php
+
+    function api_help($aRequest)
+    {
+        $Out = Out::getInstance();
+        
+        switch(strtolower($aRequest["help"]))
+        {
+        default:
+        case "help":
+            $Out->pushValue("description", "Help page.");
+            
+            $Out->pushValue("parameters", Array(
+                "topic" => "Show help on a given topic."
+            ));
+            
+            $Out->pushValue("topics", Array(
+                "help"     => "Help on help.",
+                "location" => "Help on querying locations.",
+                "raid"     => "Help on querying raid data.",
+                "stats"    => "Help on querying statistics.",
+            ));
+            break;
+            
+        case "location":
+            $Out->pushValue("description", "Get a list of available locations.");
+            $Out->pushValue("parameters", Array(
+            ));
+            break;
+            
+        case "raid":
+            $Out->pushValue("description", "Get information about raids.");
+            $Out->pushValue("parameters", Array(
+                "start"     => "Only return raids starting after this UTC timestamp. Default: 0.",
+                "end"       => "Only return raids starting before this UTC timestamp. Default: PHP_INT_MAX.",
+                "after"     => "Only return raids beginning after this hour (24h format). Default: 0.",
+                "before"    => "Only return raids beginning before this hour (24h format). Default: 0.",
+                "limit"     => "Maximum number of raids to return. Passing 0 returns all raids. Default: 10.",
+                "location"  => "Comma separated list of location names. Only returns raids on these locations. Default: empty.",
+                "full"      => "Include raids that have all slots set. Default: true.",
+                "free"      => "Include raids that do not have all slots set. Default: true.",
+                "open"      => "Include raids that are open for registration. Default: true.",
+                "closed"    => "Include raids that are closed for registration. Default: false.",
+                "canceled"  => "Include raids that have been canceled. Default: false.",
+                "attends"   => "Return list of attended players, too. Default: false.",    
+            ));
+            break;
+            
+        case "statistic":
+            $Out->pushValue("description", "Get user statistics.");
+            $Out->pushValue("parameters", Array(
+                "start"     => "Only count raids starting after this UTC timestamp. Default: 0.",
+                "end"       => "Only count raids starting before this UTC timestamp. Default: PHP_INT_MAX.",
+                "raids"     => "Comma separated list of raid ids to count. Empty counts all raids. Default: empty.",
+                "users"     => "Comma separated list of user names to include. Empty returns all users. Default: empty.",
+            ));
+            break;
+        }
+    }
+    
+?>
