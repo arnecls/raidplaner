3ce516a87edaf716c83a846a3e62b6a2c53e3ee6
Arne Claus <arne.claus@gmail.com>
2014-03-15 22:11:17 +0100

    increased patch version

--- lib/private/tools_site.php
+++ lib/private/tools_site.php
@@ -4,7 +4,7 @@
     require_once(dirname(__FILE__)."/session.class.php");
     require_once(dirname(__FILE__)."/random.class.php");
     
-    $gVersion = 110.3;
+    $gVersion = 110.4;
 
     $gSite = null;
     $gGame = null;
