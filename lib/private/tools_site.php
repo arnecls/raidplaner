0d2ef9306e23c177f39288f62e5f14e5a7cc0c93
Arne Claus <arne.claus@trivago.com>
2016-09-04 23:40:00 +0200

    Pushed version to 1.1.5a

--- lib/private/tools_site.php
+++ lib/private/tools_site.php
@@ -4,7 +4,7 @@
     require_once(dirname(__FILE__).'/session.class.php');
     require_once(dirname(__FILE__).'/random.class.php');
 
-    $gVersion = 115.0;
+    $gVersion = 115.1;
 
     $gSite = null;
     $gGame = null;
